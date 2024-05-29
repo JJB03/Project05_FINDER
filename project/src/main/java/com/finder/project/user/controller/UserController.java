8e1ca57760435aa90c59c2b9599d65b6d3daac88
tkfkd3835 <tkfkd3835@naver.com>
2024-05-29 17:46:59 +0900

    회원가입조금남음

--- project/src/main/java/com/finder/project/user/controller/UserController.java
+++ project/src/main/java/com/finder/project/user/controller/UserController.java
@@ -12,9 +12,6 @@ import com.finder.project.user.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
-
-
-
 @Slf4j
 @Controller
 @RequestMapping("/user")
@@ -22,7 +19,7 @@ public class UserController {
 
     @Autowired
     private UserService UserService;
-    
+
     @GetMapping("/{page}")
     public String main(@PathVariable("page") String page) {
         log.info("메인 화면...");
@@ -34,30 +31,36 @@ public class UserController {
 
         return "user/join_user";
     }
-    
-
 
     @PostMapping("/join_user")
     public String joinPro(Users users) throws Exception {
-
         // 생년월일의 쉼표를 "-"으로 변경
+        String userBirth = users.getUserBirth();
+        userBirth = userBirth.replace(",", "-");
+        users.setUserBirth(userBirth);
+
+        String userEmail = users.getUserEmail();
+        userEmail = userEmail.replace(",","");
+        users.setUserEmail(userEmail);
+
+        log.info("유저정보" + users);
+
         int result = UserService.join(users);
-        
+
         // 회원가입 성공
         if (result > 0) {
             return "redirect:/login";
         }
-    
+
         // 회원가입 실패
         return "redirect:/error";
     }
-    
 
     // @GetMapping("/{domain}/{page}")
     // public String main(@PathVariable("domain") String domain
-    //                   ,@PathVariable("page") String page  ) {
-    //     log.info("메인 화면...");
-    //     return domain + "/" + page;
-    // }   
-    
+    // ,@PathVariable("page") String page ) {
+    // log.info("메인 화면...");
+    // return domain + "/" + page;
+    // }
+
 }
