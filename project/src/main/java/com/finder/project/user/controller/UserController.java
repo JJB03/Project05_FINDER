919992bad11ad3fcc262be502da665f855aff068
tkfkd3835 <tkfkd3835@naver.com>
2024-05-29 20:56:25 +0900

    회원가입페이지

--- project/src/main/java/com/finder/project/user/controller/UserController.java
+++ project/src/main/java/com/finder/project/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.finder.project.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.finder.project.user.dto.Users;
 import com.finder.project.user.service.UserService;
@@ -18,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 public class UserController {
 
     @Autowired
-    private UserService UserService;
+    private UserService userService;        // 변수명은 카멜케이스로 (유상준)
 
     @GetMapping("/{page}")
     public String main(@PathVariable("page") String page) {
@@ -45,7 +48,7 @@ public class UserController {
 
         log.info("유저정보" + users);
 
-        int result = UserService.join(users);
+        int result = userService.join(users);
 
         // 회원가입 성공
         if (result > 0) {
@@ -56,11 +59,19 @@ public class UserController {
         return "redirect:/user/join_user";
     }
 
-    // @GetMapping("/{domain}/{page}")
-    // public String main(@PathVariable("domain") String domain
-    // ,@PathVariable("page") String page ) {
-    // log.info("메인 화면...");
-    // return domain + "/" + page;
-    // }
+    @ResponseBody
+    @GetMapping("/check/{userId}")
+    public ResponseEntity<Boolean> userCheck(@PathVariable("userId") String userId) throws Exception {
+        log.info("아이디 중복 확인 : " + userId);
+        Users user = userService.select(userId);
+        // 아이디 중복
+        if( user != null ) {
+            log.info("중복된 아이디 입니다 - " + userId);
+            return new ResponseEntity<>(false, HttpStatus.OK);
+        }
+        // 사용 가능한 아이디입니다.
+        log.info("사용 가능한 아이디 입니다." + userId);
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 
 }
