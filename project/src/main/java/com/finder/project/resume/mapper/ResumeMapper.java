d7a7c6d3b4039ecd4b0f37dd342696558cd7cb30
jjb <jubin030419@gmail.com>
2024-05-30 18:58:29 +0900

    mapper.xml create 부분 추가해야함

--- project/src/main/java/com/finder/project/resume/mapper/ResumeMapper.java
+++ project/src/main/java/com/finder/project/resume/mapper/ResumeMapper.java
@@ -9,24 +9,26 @@ import com.finder.project.resume.dto.Resume;
 
 @Mapper
  public interface ResumeMapper {
-          //게시글 목록 + 나중에 페이징 (아니면 스크롤로 감)
-          public List<Resume> resumelist(int userNo) throws Exception;
+    //게시글 목록 + 나중에 페이징 (아니면 스크롤로 감)
+    public List<Resume> resumelist(int userNo) throws Exception;
 
-          // 게시글 검색
-          // dto 추가 예정
-      
-          // 이력서 가져올때 리스트 여러개 가져오려면 List<Resume> 리턴받는 메서드 필요
-          //   이려서 하나 조회
-          public Resume select(int user_no) throws Exception;
-      
-          // 게시글 등록
-          public int create(Resume Resume) throws Exception;
-      
-          // 게시글 수정
-          public int update(Resume Resume) throws Exception;
-      
-          // 게시글 삭제
-          public int delete(int cv_no) throws Exception;
-      
+    // 게시글 검색
+    // dto 추가 예정
+
+    // 이력서 가져올때 리스트 여러개 가져오려면 List<Resume> 리턴받는 메서드 필요
+    //   이려서 하나 조회
+    public Resume select(int user_no) throws Exception;
+
+    // 게시글 등록
+    public int create(Resume Resume) throws Exception;
+    
+    // 게시글 수정
+    public int update(Resume Resume) throws Exception;
+    
+    // 게시글 삭제
+    public int delete(int cv_no) throws Exception;
+    
+    // pk 최댓값
+    public int maxPk() throws Exception;
 
  }
