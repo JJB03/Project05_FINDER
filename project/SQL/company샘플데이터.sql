d77cf59ac0366eb6cf86ae65fe3f437b1202db80
tkfkd3835 <tkfkd3835@naver.com>
2024-06-07 14:34:38 +0900

    새로고침 생각중

--- project/SQL/company샘플데이터.sql
+++ project/SQL/company샘플데이터.sql
@@ -1,9 +1,9 @@
--- Active: 1716771298869@@127.0.0.1@3306@joeun
+-- Active: 1716771409910@@127.0.0.1@3306@joeun
 
 INSERT INTO user( user_name, user_id, user_pw, user_birth, user_phone, user_email, user_gender, enabled) 
-VALUES ('백승헌', 'bsh1', '$2a$12$TrN..KcVjciCiz.5Vj96YOBljeVTTGJ9AUKmtfbGpgc9hmC7BxQ92', '20240101', '01012341234', 'bsh@naver.com', '남자', 2);
+VALUES ('백승헌', 'bsh', '$2a$12$TrN..KcVjciCiz.5Vj96YOBljeVTTGJ9AUKmtfbGpgc9hmC7BxQ92', '20240101', '01012341234', 'bsh@naver.com', '남자', 2);
 
-INSERT INTO user_auth (user_no, auth) VALUES (2, 'ROLE_COMPANY'); -- 기업권한
+INSERT INTO user_auth (user_no, auth) VALUES (1, 'ROLE_COMPANY'); -- 기업권한
 
 INSERT INTO company (com_name, com_category, com_address, com_business, user_no) VALUES
 ('ABC 주식회사', '소프트웨어 및 기술', '서울특별시 강남구 역삼동 123번지', '소프트웨어 개발 및 컨설팅', 1);
